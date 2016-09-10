d4e580854b0516f3596b1015787bdfb1347e581f
Abed Fayyad <AbedFayyad@live.com>
2016-09-09 22:59:06 -0400

    Rewrite Graphics class for 2D Sprite drawing

--- graphics.hpp
+++ graphics.hpp
@@ -1,23 +1,37 @@
 #ifndef graphics_hpp
 #define graphics_hpp
 
-struct SDL_Window;
-typedef void *SDL_GLContext;
-typedef unsigned int uint32_t;
-typedef uint32_t GLuint;
+#include <SDL2/SDL.h>
+#include <map>
+#include <string>
+#include <utility>
 
 class Graphics {
 public:
     Graphics();
     ~Graphics();
 
-    void draw();
+    // Load an image into memory
+    SDL_Texture *loadImage(const std::string &path);
+
+    // Unload an image from memory
+    void unloadImage(const std::string &path);
+
+    // Draw a texture to the screen
+    void draw(SDL_Texture *texture, SDL_Rect *src, SDL_Rect *dest);
+
+    // Present the contents of the renderer's backbuffer
+    void flip();
+
+    // Clear the screen - should be called before every frame is drawn
+    void clear();
+
 private:
     SDL_Window *window;
-    SDL_GLContext glcontext;
+    SDL_Renderer *renderer;
 
-    GLuint shaderProgram;
-    GLuint VAO, VBO, EBO;
+    std::map<std::string, SDL_Surface *> surfaces;
+    std::map<std::string, std::pair<SDL_Surface *, unsigned int> > textures;
 };
 
-#endif /* graphics_hpp */
+#endif
